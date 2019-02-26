a99bc7d9faabd6ffb67e0280bc2b5551c6461c3c
suhda-kumari <42617351+suhda-kumari@users.noreply.github.com>
2019-02-26 11:25:31 +0530

    Partial implementationof Quick Sort

--- Quick_Sort.java
+++ Quick_Sort.java
@@ -4,7 +4,10 @@ public class Quick_Sort implements SortingTechnique{
 	public
 	void sort(int[] a) {
 		System.out.println("Sorting with Quick Sort");
-		
+		quick_Sort(a,0,a.length-1);
 	}
-
+       void quick_Sort(int a[],int l,int r){
+       }
+       void merger(int a[],int l,int m,int r){
+       }
 }
