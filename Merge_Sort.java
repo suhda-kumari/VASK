254c4d294192aa9279e31c7f043b30b8e9674063
suhda-kumari <42617351+suhda-kumari@users.noreply.github.com>
2019-02-26 10:38:07 +0530

    Created Merge Sort Program

---
+++ Merge_Sort.java
@@ -0,0 +1,57 @@
+
+public class Merge_Sort {
+
+	public static void main(String[] args) {
+     int a[]={38,27,43,3,9,82,10};
+     mergeSort(a,0,a.length-1);
+     printArray(a);
+	}
+	static void printArray(int a[]){
+		for(int i:a)
+			System.out.print(i+" ");
+		System.out.println();
+	}
+	
+  static void mergeSort(int a[],int l,int r){
+	 
+	  if(r>l){
+		  int m=(l+r)/2;
+		  mergeSort(a,l,m);
+		  mergeSort(a,m+1,r);	  
+	     merger(a,l,m,r);
+	  }
+  }
+  static void  merger(int a[],int l,int m, int r){
+     int n1=m-l+1;
+     int n2=r-m;
+     
+     int[] L=new int[n1];
+     int[] R=new int[n2];
+      
+      
+      for(int i=0; i<n1;i++)
+    	  L[i]=a[i+l];
+      for(int j=0;j<n2;j++)
+    	  R[j]=a[m+1+j];
+    	  int i=0,j=0;
+    	  int k=l;
+    	  while(i<n1 && j<n2)
+    	  {
+    		  if(L[i]<=R[j])
+    		  { a[k]=L[i];i++;k++;}
+    		  else 
+    		  {a[k]=R[j];j++;k++;}
+    	  }
+    	  
+    	  while(i<n1)
+    	  {  a[k]=L[i];i++;k++;}
+    	  
+    	  while(j<n2)
+    	  {
+    		  a[k]=R[j];
+    		  j++;
+    		  k++;
+    	  }
+    	  
+  }
+}
